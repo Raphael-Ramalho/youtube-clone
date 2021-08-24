58dde95a8d2c7f82fd48647173c4a816e2297af3
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-08-24 19:35:59 -0300

    update

---
+++ react-materialui/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+
+function App() {
+  return (
+    <div><Button color="primary">Hello World</Button>;
+  </div>
+  );
+}
+
+export default App;
